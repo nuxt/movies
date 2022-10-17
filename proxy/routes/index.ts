7850b6c25f462693194cd20908c0c57b542007de
Pooya Parsa <pooya@pi0.io>
2022-10-17 14:29:16 +0200

    fix link

--- proxy/routes/index.ts
+++ proxy/routes/index.ts
@@ -1,1 +1,3 @@
-export default eventHandler(() => 'Nuxt Movies Proxy. Learn more: https://github.com/nuxt/movies/tree/main/proxy')
+export default eventHandler(() => {
+  return 'Nuxt Movies Proxy: <a href="https://github.com/nuxt/movies/tree/main/proxy">Learn more</a>'
+})
