24dbf614394403b819247d5ccf55fdf526383713
Pooya Parsa <pooya@pi0.io>
2022-10-17 19:47:01 +0200

    small refactors on footer for mobile display
    @antfu feel free to improve it directly

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,26 +1,36 @@
 <template>
   <footer p="y15 x15" flex="~ col gap1">
     <div row items-center gap2 my2>
-      <img src="/movies-sm.webp" width="25" height="25" alt="Nuxt Movies Logo">
+      <img src="/movies-sm.webp" width="25" height="25" alt="Logo">
       <div text-lg>
         Nuxt Movies
       </div>
-      made with
+    </div>
+    <div row items-center gap2 my2>
+      <div op50 text-sm>
+        Made with
+      </div>
       <a href="https://v3.nuxtjs.org/" target="_blank" title="Nuxt 3">
         <IconNuxt3 h6 />
       </a>
     </div>
-    <div gap2 my2 text-true-gray:90 text-sm>
-      <p>
+    <div gap2 my1>
+      <p text-true-gray:90 text-sm>
         Data provided by&nbsp;
         <a op100 target="_blank" href="https://www.themoviedb.org/" title="The Movie Database" rel="noopener" n-link-text>
-          <IconTMDB inline h6 />
+          <IconTMDB inline h4 />
         </a>
       </p>
-      <p>This project uses the TMDB API but is not endorsed or certified by TMDB.</p>
+      <p text-true-gray:90 text-sm>
+        This project uses the TMDB API but is not endorsed or certified by TMDB.
+      </p>
     </div>
-
-    <div row items-center>
+    <div>
+      <a href="https://vercel.com" rel="noopener" target="_blank" aria-label="go to vercel">
+        <IconVercel alt="Vercel" />
+      </a>
+    </div>
+    <div my3>
       <ExternalLinks
         :links="{
           twitter_id: 'nuxt_js',
@@ -28,9 +38,5 @@
         }"
       />
     </div>
-    <br>
-    <a href="https://vercel.com" rel="noopener" target="_blank" aria-label="go to vercel">
-      <IconVercel alt="Vercel" />
-    </a>
   </footer>
 </template>
