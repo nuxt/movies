6dea406647000735cd282bf9c81f58e7e5e70df8
Anthony Fu <anthonyfu117@hotmail.com>
2022-10-19 14:51:07 +0800

    feat: revert implicit flexbox

--- components/media/Details.vue
+++ components/media/Details.vue
@@ -10,7 +10,7 @@ const tab = $ref<'overview' | 'videos' | 'photos'>('overview')
 </script>
 
 <template>
-  <div row items-center justify-center gap8 py6>
+  <div flex="~ row" items-center justify-center gap8 py6>
     <button n-tab :class="{ 'n-tab-active': tab === 'overview' }" @click="tab = 'overview'">
       Overview
     </button>
