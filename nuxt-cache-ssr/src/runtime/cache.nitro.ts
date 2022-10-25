0320a40f88b8081b51e54bfffc4a8800f74db689
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2022-10-26 01:44:07 +0530

    debug

--- nuxt-cache-ssr/src/runtime/cache.nitro.ts
+++ nuxt-cache-ssr/src/runtime/cache.nitro.ts
@@ -10,7 +10,10 @@ export default defineNitroPlugin((nitroApp) => {
       const isCacheable = isUrlCacheable(event.req,options.pages)
       event.res.setHeader("x-bgv-url",`${event.req.url}`)
       event.res.setHeader("x-bgv-Cacheable",`${isCacheable}`)
+      event.res.setHeader("x-bgv-statusCode1",`${response?.statusCode}`)
+
       if(isCacheable && response.statusCode === 200){
+        event.res.setHeader("x-bgv-statusCode","true")
         const key = event.req.url
         InMemoryCache.set(key,response)
       }
