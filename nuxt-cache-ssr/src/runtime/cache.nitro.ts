b75b6e3efe8f1aadaeb410cbc3aab5034596262b
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2022-10-26 10:31:36 +0530

    Update cache.nitro.ts

--- nuxt-cache-ssr/src/runtime/cache.nitro.ts
+++ nuxt-cache-ssr/src/runtime/cache.nitro.ts
@@ -15,7 +15,13 @@ export default defineNitroPlugin( async (nitroApp) => {
       if(isCacheable && response.statusCode === 200){
         event.res.setHeader("x-bgv-statusCode","true")
         const key = event.req.url
+    const preStoredKeys = await InMemoryCache.cached?.store?.keys()
+
        await InMemoryCache.set(key,response)
+    const postStoredKeys = await InMemoryCache.cached?.store?.keys()
+    event.res.setHeader("x-pre-stored-key",`${preStoredKeys}`)
+    event.res.setHeader("x-post-stored-key",`${postStoredKeys}`)
+
       }
       
   })
