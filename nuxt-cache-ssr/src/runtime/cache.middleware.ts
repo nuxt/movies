6f251627923249a9b704e2600a4096bf518f5d28
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2022-10-26 12:12:46 +0530

    Update cache.middleware.ts

--- nuxt-cache-ssr/src/runtime/cache.middleware.ts
+++ nuxt-cache-ssr/src/runtime/cache.middleware.ts
@@ -6,19 +6,14 @@ import {isUrlCacheable} from './cache.utils'
 
 export default fromNodeMiddleware(async (req, res,next) => {
   const {url} = req
-  console.log("BGVVVV")
-  if(!isUrlCacheable(req,options.pages)){
-    next()
-  }else{
-    const cachedRes = await InMemoryCache.get(url);
-    const storedKeys = await InMemoryCache.cached?.store?.keys()
-    res.setHeader("x-cached-nuxt",`${cachedRes?.statusCode?.toString()} storedKeys: ${storedKeys}` )
-    if (!cachedRes) {
-      next()
-    } else {
+  if(isUrlCacheable(req,options.pages)){
+   const cachedRes = await InMemoryCache.get(url);
+    // const storedKeys = await InMemoryCache.cached?.store?.keys()
+    // res.setHeader("x-cached-nuxt",`${cachedRes?.statusCode?.toString()} storedKeys: ${storedKeys}` )
+    if (cachedRes) {
       res.writeHead(200, { ...cachedRes.headers, 'x-ssr-cache': 'Hit!!!' });
       res.end(cachedRes.body)
-    }
+    } 
   }
     
   })
