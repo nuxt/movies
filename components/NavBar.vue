0c6cba3df95d78f2ae513ec09ec0a9e9881dfff5
Anthony Fu <anthonyfu117@hotmail.com>
2023-01-20 12:40:06 +0100

    fix: use nuxt edge

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -6,29 +6,29 @@
     border="t lg:r base"
     bg-black
   >
-    <RouterLink v-slot="{ isActive }" to="/" title="Home">
+    <NuxtLink v-slot="{ isActive }" to="/" title="Home">
       <div
         text-2xl
         :class="isActive ? 'i-ph-house-fill text-primary' : 'i-ph-house'"
       />
-    </RouterLink>
-    <RouterLink v-slot="{ isActive }" to="/movie" title="Movies">
+    </NuxtLink>
+    <NuxtLink v-slot="{ isActive }" to="/movie" title="Movies">
       <div
         text-2xl
         :class="isActive ? 'i-ph-film-strip-fill text-primary' : 'i-ph-film-strip'"
       />
-    </RouterLink>
-    <RouterLink v-slot="{ isActive }" to="/tv" title="TV Shows">
+    </NuxtLink>
+    <NuxtLink v-slot="{ isActive }" to="/tv" title="TV Shows">
       <div
         text-2xl
         :class="isActive ? 'i-ph-television-simple-fill text-primary' : 'i-ph-television-simple'"
       />
-    </RouterLink>
-    <RouterLink v-slot="{ isActive }" to="/search" title="Search">
+    </NuxtLink>
+    <NuxtLink v-slot="{ isActive }" to="/search" title="Search">
       <div
         text-2xl
         :class="isActive ? 'i-ph-magnifying-glass-fill text-primary' : 'i-ph-magnifying-glass'"
       />
-    </RouterLink>
+    </NuxtLink>
   </div>
 </template>
