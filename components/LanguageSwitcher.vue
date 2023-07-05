74b24a41f48065ab959ce6fd83ad2f9568069e76
Ali Azimi <alibabaazimi@gmail.com>
2023-07-05 15:23:28 +0200

    feat: add language switcher option (#41)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

---
+++ components/LanguageSwitcher.vue
@@ -0,0 +1,28 @@
+<script setup>
+const { locale, locales, setLocale } = useI18n()
+
+const availableLocales = computed(() => {
+  return (locales.value)
+})
+
+function updateLocale(event) {
+  setLocale(event.target.value)
+  window.location.reload()
+}
+
+onMounted(() => {
+  const langSwitcher = document.querySelector('#langSwitcher')
+  langSwitcher.value = locale.value
+})
+</script>
+
+<template>
+  <div flex gap2 items-center mt-5>
+    Language:
+    <select id="langSwitcher" rounded-md text-sm p-1 @change="updateLocale">
+      <option v-for="loc in availableLocales" :key="loc.code" :value="loc.code" p-1>
+        {{ loc.name }}
+      </option>
+    </select>
+  </div>
+</template>
