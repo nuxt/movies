bc17352a2128d3bb65da51e30eb6fb236362d533
Anthony Fu <anthonyfu117@hotmail.com>
2023-07-05 15:23:11 +0200

    chore: update

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -2,28 +2,27 @@
 const { locale, locales, setLocale } = useI18n()
 
 const availableLocales = computed(() => {
-    return (locales.value)
+  return (locales.value)
 })
 
-const updateLocale = (event) => {
-    setLocale(event.target.value)
-    window.location.reload()
+function updateLocale(event) {
+  setLocale(event.target.value)
+  window.location.reload()
 }
 
 onMounted(() => {
-    const langSwitcher = document.querySelector('#langSwitcher')
-    langSwitcher.value = locale.value
+  const langSwitcher = document.querySelector('#langSwitcher')
+  langSwitcher.value = locale.value
 })
-
 </script>
 
 <template>
-    <div flex gap2 items-center mt-5>
-        Language:
-        <select id="langSwitcher" @change="updateLocale" rounded-md p-1>
-            <option v-for="loc in availableLocales" :key="loc.code" :value="loc.code" p-1>
-                {{ loc.name }}
-            </option>
-        </select>
-    </div>
+  <div flex gap2 items-center mt-5>
+    Language:
+    <select id="langSwitcher" rounded-md text-sm p-1 @change="updateLocale">
+      <option v-for="loc in availableLocales" :key="loc.code" :value="loc.code" p-1>
+        {{ loc.name }}
+      </option>
+    </select>
+  </div>
 </template>
