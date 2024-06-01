ae97f2589e1602afc6cf8820c91a97ed8c1cd022
Olivier Lebel <lebel.olivier.dev@gmail.com>
2024-06-01 05:25:58 +0200

    feat(i18n):translate navbar links

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -6,25 +6,25 @@
     border="t lg:r base"
     bg-black
   >
-    <NuxtLink v-slot="{ isActive }" to="/" title="Home">
+    <NuxtLink v-slot="{ isActive }" to="/" :title="$t('Home')">
       <div
         text-2xl
         :class="isActive ? 'i-ph-house-fill text-primary' : 'i-ph-house'"
       />
     </NuxtLink>
-    <NuxtLink v-slot="{ isActive }" to="/movie" title="Movies">
+    <NuxtLink v-slot="{ isActive }" to="/movie" :title="$t('Movies')">
       <div
         text-2xl
         :class="isActive ? 'i-ph-film-strip-fill text-primary' : 'i-ph-film-strip'"
       />
     </NuxtLink>
-    <NuxtLink v-slot="{ isActive }" to="/tv" title="TV Shows">
+    <NuxtLink v-slot="{ isActive }" to="/tv" :title="$t('TV Shows')">
       <div
         text-2xl
         :class="isActive ? 'i-ph-television-simple-fill text-primary' : 'i-ph-television-simple'"
       />
     </NuxtLink>
-    <NuxtLink v-slot="{ isActive }" to="/search" title="Search">
+    <NuxtLink v-slot="{ isActive }" to="/search" :title="$t('Search')">
       <div
         text-2xl
         :class="isActive ? 'i-ph-magnifying-glass-fill text-primary' : 'i-ph-magnifying-glass'"
