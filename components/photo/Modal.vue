d52c6aa2f81e5e5f5d6626119eb1a63ba5ee4e05
devinschumacher <devin@serp.co>
2024-06-11 19:07:58 -0700

    fix button missing "type" attr issue on homepage and deps

--- components/photo/Modal.vue
+++ components/photo/Modal.vue
@@ -37,7 +37,7 @@ useEventListener('keydown', (e) => {
 
 <template>
   <div v-if="images && current" fixed top-0 left-0 right-0 bottom-0 z-10 bg-black:90 p5 flex items-center justify-center>
-    <button absolute top-1 right-1 z-100 p3 text-3xl n-link bg-black:60 rounded-full @click="images = null">
+    <button type="button" absolute top-1 right-1 z-100 p3 text-3xl n-link bg-black:60 rounded-full @click="images = null">
       <div i-carbon-close />
     </button>
     <NuxtImg
@@ -48,12 +48,12 @@ useEventListener('keydown', (e) => {
       max-w-full max-h-full object-contain
     />
     <div absolute left-0 top="1/2" translate="y--1/2">
-      <button py10 px4 bg-black:30 op10 hover:op100 @click="prev()">
+      <button type="button" py10 px4 bg-black:30 op10 hover:op100 @click="prev()">
         <div i-ph-caret-left-light text-3xl text-white />
       </button>
     </div>
     <div absolute right-0 top="1/2" translate="y--1/2">
-      <button py10 px4 bg-black:30 op10 hover:op100 @click="next()">
+      <button type="button" py10 px4 bg-black:30 op10 hover:op100 @click="next()">
         <div i-ph-caret-right-light text-3xl text-white />
       </button>
     </div>
