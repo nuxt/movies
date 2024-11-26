5521d9cefef1d1ef7455e77c0095bfe3dbca2f75
Martin Masevski <martin.masevski@gmail.com>
2024-11-26 17:00:22 +0100

    feat: init e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,10 @@
+import process from 'node:process'
 import { defineConfig } from '@playwright/test'
 import type { ConfigOptions } from '@nuxt/test-utils/playwright'
 
 export default defineConfig<ConfigOptions>({
   testMatch: '**/tests/e2e/*.spec.ts',
+  use: {
+    baseURL: process.env.BASE_URL,
+  },
 })
