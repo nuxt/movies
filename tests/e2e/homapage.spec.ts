9f1c7ad9c56b23a5ccc3a840867d9f69028906d5
Martin Masevski <martin.masevski@gmail.com>
2024-11-27 17:15:52 +0100

    fix: update homepage test headings to reflect correct title

--- tests/e2e/homapage.spec.ts
+++ tests/e2e/homapage.spec.ts
@@ -2,10 +2,10 @@ import { expect, test } from '@nuxt/test-utils/playwright'
 
 test('homepage displays correctly 1', async ({ page, goto }) => {
   await goto('', { waitUntil: 'hydration' })
-  await expect(page.getByRole('heading')).toContainText('test1', { ignoreCase: true })
+  await expect(page.getByRole('heading')).toContainText('Venom: The Last Dance', { ignoreCase: true })
 })
 
 test('homepage displays correctly 2', async ({ page, goto }) => {
   await goto('', { waitUntil: 'hydration' })
-  await expect(page.getByRole('heading')).toContainText('test2', { ignoreCase: true })
+  await expect(page.getByRole('heading')).toContainText('Venom: The Last Dance', { ignoreCase: true })
 })
