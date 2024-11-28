f3fc4457a312952b659f5dff69261823ee4bf49f
Martin Masevski <martin.masevski@gmail.com>
2024-11-28 10:12:01 +0100

    feat: add mock media and tests for media handling functions

--- vitest.component.config.ts
+++ vitest.component.config.ts
@@ -2,7 +2,7 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
-    include: ['**/components/**/*.test.ts'],
+    include: ['**/components/**/*.test.ts', '**/composables/**/*.test.ts'],
     name: 'component',
     environment: 'node',
   },
