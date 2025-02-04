1a2ece2c43a5b57ec282fe3e543f0d6571533221
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2025-02-04 15:39:13 +0330

    firefox-like scrollbar in chrome

--- app.vue
+++ app.vue
@@ -1,37 +1,33 @@
 <script setup>
-import '@unocss/reset/tailwind.css'
+import "@unocss/reset/tailwind.css";
 
 useHead({
   htmlAttrs: {
-    lang: 'en',
+    lang: "en"
   },
-  charset: 'utf-8',
-  title: 'Nuxt Movies',
-  titleTemplate: title => title !== 'Nuxt Movies' ? `${title} · Nuxt Movies` : title,
+  charset: "utf-8",
+  title: "Nuxt Movies",
+  titleTemplate: title => (title !== "Nuxt Movies" ? `${title} · Nuxt Movies` : title),
   meta: [
-    { name: 'description', content: 'A TMDB client built with Nuxt Image to show the potential of it ✨' },
-    { property: 'og:image', content: 'https://movies.nuxt.space/social-card.png' },
-    { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:site', content: '@nuxt_js' },
-    { name: 'twitter:creator', content: '@nuxt_js' },
+    {name: "description", content: "A TMDB client built with Nuxt Image to show the potential of it ✨"},
+    {property: "og:image", content: "https://movies.nuxt.space/social-card.png"},
+    {name: "twitter:card", content: "summary_large_image"},
+    {name: "twitter:site", content: "@nuxt_js"},
+    {name: "twitter:creator", content: "@nuxt_js"}
   ],
   link: [
     {
-      rel: 'icon',
-      type: 'image/webp',
-      href: '/movies.webp',
-    },
-  ],
-})
+      rel: "icon",
+      type: "image/webp",
+      href: "/movies.webp"
+    }
+  ]
+});
 </script>
 
 <template>
   <NuxtLoadingIndicator />
-  <div
-    h-full w-full font-sans
-    grid="~ lt-lg:rows-[1fr_max-content] lg:cols-[max-content_1fr]"
-    of-hidden view-transition-app transition duration-0
-  >
+  <div h-full w-full font-sans grid="~ lt-lg:rows-[1fr_max-content] lg:cols-[max-content_1fr]" of-hidden view-transition-app transition duration-0>
     <div id="app-scroller" of-x-hidden of-y-auto relative>
       <NuxtPage />
     </div>
@@ -42,12 +38,29 @@ useHead({
 </template>
 
 <style>
-html, body , #__nuxt{
+html,
+body,
+#__nuxt {
   height: 100vh;
   margin: 0;
   padding: 0;
   background: #111;
   color: white;
   color-scheme: dark;
+}
+
+::-webkit-scrollbar {
+  width: 7px;
+  height: 5px;
+}
+::-webkit-scrollbar-track {
+  box-shadow: none;
+}
+::-webkit-scrollbar-track:hover {
+  background-color: #2d2b2b;
+}
+::-webkit-scrollbar-thumb {
+  background: #959595;
+  border-radius: 10px;
 }
 </style>
