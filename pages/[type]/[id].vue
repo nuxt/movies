359075c06ee11662874411dc3bb36e11c25bde1b
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2025-02-05 16:44:21 +0330

    show a card on media-card-hover

--- pages/[type]/[id].vue
+++ pages/[type]/[id].vue
@@ -1,30 +1,28 @@
 <script setup lang="ts">
-import type { MediaType } from '~/types'
+import type {MediaType} from "~/types";
 
 definePageMeta({
   key: route => route.fullPath,
-  validate: ({ params }) => {
-    return ['movie', 'tv'].includes(params.type as MediaType)
-  },
-})
+  validate: ({params}) => {
+    return ["movie", "tv"].includes(params.type as MediaType);
+  }
+});
 
-const route = useRoute()
-const type = computed(() => route.params.type as MediaType || 'movie')
-const id = computed(() => route.params.id as string)
+const loadingIndicator = useLoadingIndicator();
+const route = useRoute();
+const type = computed(() => (route.params.type as MediaType) || "movie");
+const id = computed(() => route.params.id as string);
 
-const [item, recommendations] = await Promise.all([
-  getMedia(type.value, id.value),
-  getRecommendations(type.value, id.value),
-])
-const $img = useImage()
+const [item, recommendations] = await Promise.all([getMedia(type.value, id.value), getRecommendations(type.value, id.value)]);
+const $img = useImage();
 
 useHead({
   title: item.name || item.title,
   meta: [
-    { name: 'description', content: item.overview },
-    { property: 'og:image', content: $img(`/tmdb${item.poster_path}`, { width: 1200, height: 630 }) },
-  ],
-})
+    {name: "description", content: item.overview},
+    {property: "og:image", content: $img(`/tmdb${item.poster_path}`, {width: 1200, height: 630})}
+  ]
+});
 </script>
 
 <template>
@@ -33,15 +31,9 @@ useHead({
     <MediaDetails :item="item" :type="type" />
     <CarouselBase v-if="recommendations?.results?.length">
       <template #title>
-        {{ $t('More like this') }}
+        {{ $t("More like this") }}
       </template>
-      <MediaCard
-        v-for="i of recommendations.results"
-        :key="i.id"
-        :item="i"
-        :type="type"
-        flex-1 w-40 md:w-60
-      />
+      <MediaCard v-for="i of recommendations.results" :key="i.id" :item="i" :type="type" flex-1 w-40 md:w-60 />
     </CarouselBase>
     <TheFooter />
   </div>
