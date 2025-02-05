5adfe6f8a3440f7291104e733a7dbf932c81bf8d
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2025-02-05 16:45:12 +0330

    show a card on media-card-hover

--- components/media/Card.vue
+++ components/media/Card.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
-import type { Media, MediaType, QueryItem } from '~/types'
+import type {Media, MediaType, QueryItem} from "~/types";
 
 defineProps<{
-  type: MediaType
-  item: Media,
-  query?: QueryItem
-}>()
+  type: MediaType;
+  item: Media;
+  query?: QueryItem;
+}>();
+
+let isHover = ref();
 </script>
 
 <template>
-  <NuxtLink
-    :to="`/${item.media_type || type}/${item.id}`" pb2
-  >
-    <div
-      block bg-gray4:10 p1
-      class="aspect-10/16"
-      transition duration-400
-      hover="scale-105 z10"
-    >
+  <NuxtLink :to="`/${item.media_type || type}/${item.id}`" pb2 relative @mouseover="isHover = true" @mouseleave="isHover = false">
+    <Transition>
+      <MediaCardHover v-if="isHover" :item="item" />
+    </Transition>
+    <div block bg-gray4:10 p1 class="aspect-10/16" transition duration-400 hover="scale-105 z10">
       <NuxtImg
         v-if="item.poster_path"
         width="400"
@@ -25,9 +23,10 @@ defineProps<{
         format="webp"
         :src="`/tmdb${item.poster_path}`"
         :alt="item.title || item.name"
-        w-full h-full object-cover
-        :style="{ 'view-transition-name': `item-${item.id}${query?.query ? `-${query.query}` : ''}` }"
-      />
+        w-full
+        h-full
+        object-cover
+        :style="{'view-transition-name': `item-${item.id}${query?.query ? `-${query.query}` : ''}`}" />
       <div v-else h-full op10 flex>
         <div i-ph:question ma text-4xl />
       </div>
@@ -43,3 +42,15 @@ defineProps<{
     </div>
   </NuxtLink>
 </template>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ components/media/CardHover.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import type { Media } from "~/types";
+
+defineProps<{
+  item: Media
+}>()
+</script>
+
+<template>
+  <div absolute inset-0 class="z-[1000] bg-[#1F2021] rounded-md w-[calc(200%+8px)]" transition-all left-0>
+    <div flex p-2 gap-2 items-center size-full class="*:h-full">
+      <div class="w-[50%] h-full">
+        <NuxtImg
+          v-if="item.poster_path"
+          width="400"
+          height="600"
+          format="webp"
+          :src="`/tmdb${item.backdrop_path}`"
+          :alt="item.title || item.name"
+          w-full
+          h-full
+          object-cover
+          :style="{'view-transition-name': `item-${item.id}${query?.query ? `-${query.query}` : ''}`}" />
+        <div v-else h-full op10 flex>
+          <div i-ph:question ma text-4xl />
+        </div>
+      </div>
+      <div class="w-1/2" flex gap-2 flex-col h-full>
+        <p text-2xl font-bold>
+          {{ item.title }}
+        </p>
+        <p line-clamp-9 class="leading-[28px]">
+          {{ item.overview }}
+        </p>
+        <p>
+          Released
+          {{ new Date(item.release_date).toLocaleDateString() }}
+        </p>
+        <div class="flex gap-1 flex-col">
+          <div class="flex gap-1 items-center">
+            <StarsRate w-20 :value="item.vote_average" />
+            <span class="op50 text-sm">
+              {{ formatVote(item.vote_average) }}
+            </span>
+          </div>
+          <div class="op50 text-sm">
+            {{ $t("{numberOfReviews} Reviews", {numberOfReviews: formatVote(item.vote_count)}) }}
+          </div>
+        </div>
+        {{ console.log(item) }}
+      </div>
+    </div>
+  </div>
+</template>
