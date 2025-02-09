a1c517d7cf083a082f572b6d817227ceab819b73
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2025-02-09 10:00:51 +0330

    add loading for trailer load & separate loading component

--- components/IframeModal.vue
+++ components/IframeModal.vue
@@ -1,39 +1,32 @@
 <script setup lang="ts">
-import { onKeyDown } from '@vueuse/core'
+import {onKeyDown} from "@vueuse/core";
 
-const src = ref<string | null>(null)
+const loading = ref(true);
+const src = ref<string | null>(null);
 
 function showModal(link: string) {
-  src.value = link
+  src.value = link;
+  loading.value = true;
 }
-const el = ref<HTMLIFrameElement>()
+const el = ref<HTMLIFrameElement>();
 
-provideIframeModal(showModal)
+provideIframeModal(showModal);
 
-onKeyDown('Escape', () => {
-  if (src.value)
-    src.value = null
-})
+onKeyDown("Escape", () => {
+  if (src.value) src.value = null;
+});
 
 onClickOutside(el, () => {
-  src.value = null
-})
+  src.value = null;
+});
 </script>
 
 <template>
   <div v-if="src" fixed top-0 left-0 right-0 bottom-0 z-10 bg-black:90 flex>
-    <button
-      absolute top-1 right-1 z-100 p3 text-3xl n-link bg-black:60 rounded-full
-      title="Close"
-      @click="src = null"
-    >
+    <button absolute top-1 right-1 z-100 p3 text-3xl n-link bg-black:60 rounded-full title="Close" @click="src = null">
       <div i-carbon-close />
     </button>
-    <iframe
-      ref="el"
-      allow="autoplay; encrypted-media"
-      allowfullscreen
-      :src="src" w-full m5 lg:m20 border-none
-    />
+    <iframe ref="el" allow="autoplay; encrypted-media" allowfullscreen :src="src" w-full m5 lg:m20 border-none @load="loading = false" />
+    <Loading v-if="loading" />
   </div>
 </template>
---
+++ components/Loading.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="fixed inset-0 z-[1000] flex items-center justify-center">
+    <div class="z-0 fixed inset-0 bg-blur bg-[rgba(#8a2a0a,0.25)]" />
+    <div class="z-1 bg-black px-6 py-2 rounded-lg">
+      <img class="w-12 loader" src="/movies.webp" width="25" height="25" alt="Logo" />
+    </div>
+  </div>
+</template>
