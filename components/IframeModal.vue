83dfdd7a0d281a041f3630f8f46e233d8b253d83
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2025-02-09 10:14:46 +0330

    handle iframe error

--- components/IframeModal.vue
+++ components/IframeModal.vue
@@ -7,6 +7,11 @@ const src = ref<string | null>(null);
 function showModal(link: string) {
   src.value = link;
   loading.value = true;
+
+  // handle iframe load error
+  setTimeout(() => {
+    loading.value = false;
+  }, 7000);
 }
 const el = ref<HTMLIFrameElement>();
 
@@ -23,7 +28,7 @@ onClickOutside(el, () => {
 
 <template>
   <div v-if="src" fixed top-0 left-0 right-0 bottom-0 z-10 bg-black:90 flex>
-    <button absolute top-1 right-1 z-100 p3 text-3xl n-link bg-black:60 rounded-full title="Close" @click="src = null">
+    <button absolute top-1 right-1 z-100 p3 text-3xl n-link bg-black:60 rounded-full title="Close" @click="src = null" @error="console.log('error')" onerror="console.log('error')">
       <div i-carbon-close />
     </button>
     <iframe ref="el" allow="autoplay; encrypted-media" allowfullscreen :src="src" w-full m5 lg:m20 border-none @load="loading = false" />
